82db9186f0352a0c1aa2f8b6007dbb459baf2d11
Patel <patelde@tbdir.net>
2025-02-20 10:42:58 +0530

    TODO

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+import os
+import pyodbc
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+# Fetch connection string from Azure environment variables
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Connect to Azure SQL Database
+conn = pyodbc.connect(DATABASE_URL)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tasks")
+    tasks = cursor.fetchall()
+    return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
